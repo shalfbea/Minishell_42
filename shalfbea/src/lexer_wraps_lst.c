7e2d5bc6e60a619b1e6a0194cd9eb6f4ce808e38
shalfbea <shalfbea@student.21-school.ru>
2022-04-27 21:08:02 +0300

    modified:   Makefile 	new file:   src/lexer_wraps_lst.c 	modified:   src/prompt.c 	deleted:    ../Makefile 	modified:   ../include/shalfbea_edit.h 	deleted:    ../src/main.c

---
+++ shalfbea/src/lexer_wraps_lst.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_wraps_lst.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shalfbea <shalfbea@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 19:41:36 by shalfbea          #+#    #+#             */
+/*   Updated: 2022/04/27 20:49:09 by shalfbea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	type_detector(char	*str)
+{
+	/*{">", "<", ">>", "<<", "|", "&&", "||"}; */
+	if (str[0] == '>' && !str[1])
+		return (REDIR_OUT);
+	if (str[0] == '<' && !str[1])
+		return (REDIR_IN);
+	if (str[0] == '>' && str[1] == '>' && !str[2])
+		return (REDIR_APPEND);
+	if (str[0] == '<' && str[1] == '<' && !str[2])
+		return (REDIR_INSOURCE);
+	if (str[0] == '|' && !str[1])
+		return (PIPE);
+	if (str[0] == '&' && str[1] == '&' && !str[2])
+		return (IF_AND);
+	if (str[0] == '|' && str[1] == '|' && !str[2])
+		return (IF_OR);
+	return (0);
+}
+
+void	add_to_lexer(t_list **lst, char *str, char type)
+{
+	t_lexer	*lex;
+
+	lex = malloc(sizeof(t_lexer));
+	if (!lex)
+		return ; //RAISE ERROR or exit
+	lex->str = str;
+	if (type == '\'')
+		type = QUOTES;
+	else if (type == '"')
+		type = DOUBLE_QUOTES;
+	else
+		type = type_detector(str);
+	lex->type = type;
+	ft_lstadd_back(lst, ft_lstnew((void *) lex));
+}
+
+void	lexer_content_deleter(void *content)
+{
+	t_lexer	*lex;
+
+	lex = (t_lexer *) content;
+	if (!lex)
+		return ;
+	if (lex->str)
+		free(lex->str);
+	free(lex);
+	return ;
+}
+
+t_list	*clear_lexer_lst(t_list **lst)
+{
+	ft_lstclear(lst, lexer_content_deleter);
+	return (NULL);
+}
--- shalfbea/src/prompt.c
+++ shalfbea/src/prompt.c
@@ -6,46 +6,74 @@
 /*   By: shalfbea <shalfbea@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 15:56:44 by shalfbea          #+#    #+#             */
-/*   Updated: 2022/04/27 17:38:28 by shalfbea         ###   ########.fr       */
+/*   Updated: 2022/04/27 21:06:10 by shalfbea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_list	*splitter(char *str)
+char	error_msg(int mode)
+{
+	if (mode == QUOTES)
+		printf("Error while parsing: quotes unmatched.");
+	return (1);
+}
+
+char	quotes_breaker(char *str, size_t *i, t_list	**res)
+{
+	size_t	begin;
+	char	quote;
+
+	quote = str[*i];
+	(*i)++;
+	if (!str[*i])
+		return (error_msg(QUOTES)); //raise error!
+	begin = *i;
+	while (str[*i] && str[*i] != quote)
+		++(*i);
+	if (str[*i] != quote)
+		return (error_msg(QUOTES)); //raise error!
+	add_to_lexer(res, ft_substr(str, begin, *i - begin), quote);
+	//++(*i);
+	return (0);
+}
+
+t_list	*splitter(char *str, size_t i, size_t begin)
 {
 	t_list			*res;
-	size_t			end;
 	unsigned char	is_word;
-	size_t			i;
-	size_t			begin;
 
-	end = 0;
-	i = -1;
 	res = NULL;
 	while (str[++i])
 	{
-		if (!ft_isspace(str[i]) && (!is_word))
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			if (quotes_breaker(str, &i, &res))
+				return (clear_lexer_lst(&res));
+		}
+		else if (!ft_isspace(str[i]) && (!is_word) && str[i])
 		{
 			begin = i;
 			is_word = 1;
 		}
-		if (ft_isspace(str[i]) && is_word)
+		else if (ft_isspace(str[i]) && is_word)
 		{
 			is_word = 0;
-			ft_lstadd_back(&res, ft_lstnew((void *) ft_substr(str, begin, end + 1)));
+			add_to_lexer(&res, ft_substr(str, begin, i - begin), 0);
 		}
-		if (is_word)
-			end = i;
+		printf("%zu", i);
 	}
+	printf("\n");
 	if (is_word)
-		ft_lstadd_back(&res, ft_lstnew((void *) ft_substr(str, begin, end + 1)));
+		add_to_lexer(&res, ft_substr(str, begin, i - begin), 0);
 	return (res);
 }
 
-void iter_printer(void *cur)
+void	iter_printer(void *cur)
 {
-	printf("%s;", (char *) cur);
+	char	*types[] = {"word", "in quotes", "in double quotes", "redir out >", "redir in <", "redir append >>", "redir insource <<", "pipe |", "if_and &&", "if_or ||"};
+
+	printf("%s;type: %s\n", ((t_lexer *) cur)->str, types[(int) ((t_lexer *) cur)->type]);
 }
 
 int	prompt(void)
@@ -54,13 +82,15 @@ int	prompt(void)
 	t_list	*args;
 
 	str = NULL;
+	//str = "\"\'lol\'\"";
 	str = readline("MiniShell: ");
 	if (!str)
 		exit(0); // затычка
-	args = splitter(str);
+	args = splitter(str, -1, 0);
 	ft_lstiter(args, iter_printer);
 	printf("\n");
-	if (str)
-		free(str);
+	//if (str)
+	//	free(str);
+	pause();
 	return (0);
 }
