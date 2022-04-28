9f959a803fa0608965ed7a2ab32c83e66e06de5e
Coach Bridget <cbridget@oa-d6.msk.21-school.ru>
2022-04-28 19:44:05 +0300

    some more changes

---
+++ cbridget/src/executor.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbridget <cbridget@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 17:22:45 by cbridget          #+#    #+#             */
+/*   Updated: 2022/04/28 18:44:28 by cbridget         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	executor(t_executor_environment *commands)
+{
+	int	*fds;
+
+	fds = 
+	if (open_files())
+		return (1);
+}
+
+int	open_files(t_command_list *commands, )
+{
+	
+}
