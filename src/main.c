5ddd58f89683e8f923a3f6d64f250723cf663928
Selka Halfbeak <shalfbea@pl-g1.msk.21-school.ru>
2022-06-27 20:32:37 +0300

    -c argument support, for testing with python module

--- src/main.c
+++ src/main.c
@@ -6,13 +6,31 @@
 /*   By: shalfbea <shalfbea@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 19:23:41 by shalfbea          #+#    #+#             */
-/*   Updated: 2022/06/23 21:09:18 by shalfbea         ###   ########.fr       */
+/*   Updated: 2022/06/27 20:25:01 by shalfbea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 t_minishell_environment g_ms_env;
+
+static int	c_argument_support(int argc, char **argv)
+{
+	t_command_list	*commands;
+	t_list			*raw_lexer_data;
+
+	if (argc < 3)
+		return (0);
+	if (ft_strncmp(argv[1], "-c", 2))
+		return (0);
+	g_ms_env.prompt_mode = 1;
+	raw_lexer_data = prompt(argv[2]);
+	g_ms_env.prompt_mode = 0;
+	commands = get_command(raw_lexer_data);
+	executor(commands);
+	clear_lexer_lst(&(raw_lexer_data));
+	return (SHELL_CLOSE);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -26,7 +44,8 @@ int	main(int argc, char **argv, char **envp)
 	if (ms_env_initter(envp))
 		exit(1);
 	set_sig_control();
-	executor_result = 0;
+	//debug_set_envp(); // Delete
+	executor_result = c_argument_support(argc, argv);// 0;
 	while (executor_result != SHELL_CLOSE)
 	{
 		g_ms_env.prompt_mode = 1;
@@ -37,8 +56,10 @@ int	main(int argc, char **argv, char **envp)
 			break ;
 		executor_result = executor(commands);
 		clear_lexer_lst(&(raw_lexer_data));
+		if (S_DEBUG == 1)
+			executor_result = SHELL_CLOSE;
 	}
 	string_array_cleaner(&g_ms_env.envp);
-	//return (g_ms_env.ex_code);
+	//pause();
 	return (g_ms_env.ex_code);
 }
